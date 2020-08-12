90bf42ee6a56931216163fa7be5c2bd1290793c7
callinghome <c54071089@gs.ncku.edu.tw>
2020-08-12 18:22:03 +0800

    ATM

---
+++ src/main/java/com/pcschool/java/ATM/Account.java
@@ -0,0 +1,25 @@
+
+package com.pcschool.java.ATM;
+
+public class Account {
+    private int balance;
+    
+    // 存款
+    public void save(int money) {
+        if(money > 0) {
+            balance += money; // balance = balance + money
+        }
+    }
+    
+    // 提款
+    public void withdraw(int money) {
+        if(money > 0 && balance >= money) {
+            balance -= money; // balance = balance - money
+        }
+    }
+    
+    // 取得帳戶餘額
+    public int getBalance() {
+        return balance;
+    }
+}
